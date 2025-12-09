0d649f6559902049b45c933a919559c67697ebd0
oliviarainin28 <orainin28@bentleyschool.org>
2025-12-08 23:27:14 -0800

    Add files via upload

--- CSP_4_01_Basics_of_Loops.py
+++ CSP_4_01_Basics_of_Loops.py
@@ -7,8 +7,13 @@ def oddNumbers(n:int) ->str:
     example oddNumbers(8) -> "1 3 5 7"
     example oddNumbers(-8) -> ""
     """
-
-
+    answer = ""
+    for x in range(1,n+1):
+        if x%2 ==1:
+            answer += str(x)
+            if x+2 <= n:
+                answer += " "
+    return answer
 def backwards(n)-> int:
     """
     modify the below function such that it prints out all the numbers from n to 1
@@ -17,8 +22,15 @@ def backwards(n)-> int:
     example backwards(8) -> "8 7 6 5 4 3 2 1"
     example backwards(-2) -> ""
     """
+    if n < 1:
+       return ""
 
+    result = str(n)
 
+    for x in range(n-1,0,-1):
+       result += " " + str(x)
+
+    return result
 
 def randomRepeating():
     """
@@ -28,7 +40,15 @@ def randomRepeating():
     :return:
     """
     tries = 0
-    print(f"it took {tries} tries to get a 10")
+    #print(random.randint(1, 10))
+    #print(f"it took {tries} tries to get a 10")
+    num = 0
+
+    while num != 10:
+        num = random.randint(1,10)
+        tries += 1
+    print(f"It took {tries} to get a 10")
+
 def randomRange(n):
     """
     Generate a random number from 1 to 100 n number of times. Then after that is
@@ -37,12 +57,23 @@ def randomRange(n):
     :param n:
     :return:
     """
+    nums = []
+    for _ in range(n):
+        nums.append(random.randint(1,100))
+
+    print("Highest:", max(nums))
+    print("Lowest:", min(nums))
+
 def reverse(word:str)->str:
     """
     Takes in a string as an argument and return the given string in reverse.
     example reverse("cat") -> "tac"
     example reverse("Hello") -> "olleH"
     """
+    answer = ""
+    for c in word:
+        answer = c + answer
+    return answer
 
 def fizzBuzzContinuous(n):
     """
@@ -58,7 +89,23 @@ def fizzBuzzContinuous(n):
     :param n:
     :return:
     """
+    answer = ""
+    first = True
 
+    for x in range(1, n+1):
+        if not first:
+            answer += " "
+        if x%3 == 0 and x%5 == 0:
+            answer += "fizzbuzz"
+        elif x%3 == 0:
+            answer += "fizz"
+        elif x%5 == 0:
+            answer += "buzz"
+        else:
+            answer += str(x)
+        first = False
+    return answer
+    return answer
 def collatz(n):
     """
     Modify this function such that it mimics the collatz conjecture starting at n
@@ -69,12 +116,20 @@ def collatz(n):
     :param n:
     :return:
     """
+    result = str(n)
 
+    while n != 1:
+        if n%2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        result += " " + str(n)
+    return result
 
 def fibonacci(n):
     """
     for the given argument n print out the first n numbers of the fibonacci
-    sequence in a single string sperated by spaces.
+    sequence in a single string seperated by spaces.
     The fibonacci sequence is defined as a sequence that starts with 0 then 1 as
     the first two numbers. Every subsequent number is the prior two numbers added together.
     Example fibonacci(6) -> "0 1 1 2 3 5"
@@ -83,6 +138,27 @@ def fibonacci(n):
     :param n:
     :return:
     """
+    if n <= 0:
+        return ""
+    if n == 1:
+        return "0"
+    a = 0
+    b = 1
+    answer = "0 1"
+    count = 2
 
+    while count < n:
+        c = a + b
+        answer += " " + str(c)
+        a = b
+        b = c
+        count += 1
+    return answer
 
-print(fibonacci(300))
+def test_odd_numbers():
+    assert oddNumbers(5) == "1 3 5"
+    assert oddNumbers(8) == "1 3 5 7"
+    assert oddNumbers(-8) == ""
+    assert oddNumbers(80) == "1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51 53 55 57 59 61 63 65 67 69 71 73 75 77 79"
+
+test_odd_numbers()
